85884f123023bde4ecfe1440bd27b7ab3e820b0a
Damini <damini.2201@gmail.com>
2021-12-02 00:28:18 +0530

    Added : Create a getMessage method to demonstrate GET request method to print message

--- src/main/java/com/bridgelabz/firstspringapp/controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/firstspringapp/controller/HelloRestController.java
@@ -3,6 +3,7 @@ package com.bridgelabz.firstspringapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,4 +20,8 @@ public class HelloRestController {
         return "Hello From Bridgelabz......";
     }
 
+    @GetMapping("/hello")
+    public String getMessage() {
+        return "Hello From Bridgelabz......";
+    }
 }
