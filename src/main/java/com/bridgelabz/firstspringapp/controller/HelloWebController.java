3200e9579751d2cb543294eafc87178a10284126
Damini <damini.2201@gmail.com>
2021-12-01 21:35:12 +0530

    Added : Create a MVC controller and HTML pages to display message on a webpage

--- src/main/java/com/bridgelabz/firstspringapp/controller/HelloWebController.java
+++ src/main/java/com/bridgelabz/firstspringapp/controller/HelloWebController.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.firstspringapp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Purpose : To Create RestController to print hello message.
@@ -9,11 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
  * @since  : 01-12-2021
  */
 
-@RestController
+@Controller
 public class HelloWebController {
 
     @GetMapping("/web")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/web/message")
+    public String message(Model model) {
+        model.addAttribute("message","This is a custome message");
+        return "message";
     }
 }
